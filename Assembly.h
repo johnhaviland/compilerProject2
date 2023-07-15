cc5bd82fb566e67df54ec54e6abf54b8ad695a2d
johnhaviland <138923412+johnhaviland@users.noreply.github.com>
2023-07-14 22:51:20 -0700

    Assembly.h

--- Assembly.h
+++ Assembly.h
@@ -1,6 +1,6 @@
+// File creates assembly code to be run in MIPS (QTSpim)
 #include <stdio.h>
-// Set of functions to emit MIPS code
-FILE * MIPScode;
+
 void  initAssemblyFile(){
     printf("accessed initAssemblyFile");
     FILE * MIPScode;
@@ -9,34 +9,30 @@ void  initAssemblyFile(){
     
     fprintf(MIPScode, ".text\n");
     fprintf(MIPScode, "main:\n");
-    fprintf(MIPScode, "# -----------------------\n");
-
+    fprintf(MIPScode, "# ==================================\n");
 }
 
 void emitMIPSAssignment(char * id1, char * id2){
-
   fprintf(MIPScode, "li $t1,%s\n", id1);
   fprintf(MIPScode, "li $t2,%s\n", id2);
   fprintf(MIPScode, "li $t2,$t1\n");
 }
-int anything = 0;
-void emitMIPSConstantIntAssignment (char id1[50], char id2[50],int curScope[50]){
 
+void emitMIPSConstantIntAssignment (char id1[50], char id2[50],int curScope[50]){
     fprintf(MIPScode, "li $t%d,%s\n",curScope, id2);
 }
 
 void emitMIPSWriteId(char * id, int count){
         fprintf(MIPScode, "move $a0,$t%d\n", count);
-        fprintf(MIPScode, "li $v0,1   # call code for terminate\n");
-        fprintf(MIPScode, "syscall      # system call (terminate)\n");
+        fprintf(MIPScode, "li $v0,1\n");
+        fprintf(MIPScode, "syscall\n");
         fprintf(MIPScode, "li $a0, 10\nli $v0, 11\nsyscall\n\n");
 }
 
 void emitEndOfAssemblyCode(){
-    fprintf(MIPScode, "# -----------------\n");
-    fprintf(MIPScode, "#  Done, terminate program.\n\n");
-    fprintf(MIPScode, "li $v0,10   # call code for terminate\n");
-    fprintf(MIPScode, "syscall      # system call (terminate)\n");
+    fprintf(MIPScode, "# ==================================\n");
+    fprintf(MIPScode, "li $v0,10\n");
+    fprintf(MIPScode, "syscall\n");
     fprintf(MIPScode, ".end main\n");
 }
     
