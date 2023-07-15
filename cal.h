eea9908890a7a3ae14b9a19fa90904f73638e7fc
johnhaviland <138923412+johnhaviland@users.noreply.github.com>
2023-07-14 22:54:37 -0700

    cal.h

--- cal.h
+++ cal.h
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int ArrayofNumbers[10];
-int cuurentIndex = 0;
+int curentIndex = 0;
 
 void addItems(int item)
 {
     ArrayofNumbers[cuurentIndex] = item;
-    cuurentIndex++;
+    curentIndex++;
 }
 
-int sumOfItems()
-{
+int sumOfItems(){
     int sum = 0;
-    for (int i = 0; i < 50; i++)
-    {
+    for (int i = 0; i < 50; i++){
         sum += ArrayofNumbers[i];
         ArrayofNumbers[i] = 0;
     }
+    
     return sum;
 }
